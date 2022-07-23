e0a426b22239c9ed0db992f3513c3d68e0777e26
Vitor Jaguat <vitorbutkus@gmail.com>
2022-07-23 13:35:41 -0300

    Add Workout class, refactor

--- script.js
+++ script.js
@@ -11,14 +11,67 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class Workout {
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
 
+    constructor(coords, distance, duration) {
+        this.coords = coords; // [lat, lng]
+        this.distance = distance; //in km
+        this.duration = duration; //in min
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace() {
+        // min/km
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        // km/h
+        this.speed = this.distance / (this.durantion / 60);
+        return this.speed;
+    }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycling1);
+
+
+
+//////////////////////////////////////////
+// APPLICATION ARCHITETURE
 class App {
-    constructor() { }
+    #map;
+    #mapEvent;
+
+    constructor() {
+        this._getPosition();
+
+        form.addEventListener('submit', this._newWorkout.bind(this));
+
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
 
     _getPosition() {
         if (navigator.geolocation)
-            navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
                 alert('Could not get your position!');
             }
             );
@@ -37,58 +90,52 @@ class App {
         //Displaying Leaflet map: (code available on https://leafletjs.com/index.html )
         //'L' is the Naming space used by the Leaflet script. 'map', 'tileLayer', 'marker' are methods that the Leaflet object can use.
         //'map' is the id of a div in HTML, where the map should appear.
-        map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-
+        }).addTo(this.#map);
 
         //Handling clicks on map:
+        this.#map.on('click', this._showForm.bind(this));
+    }
+
+    _showForm(mapE) {
         //'on' is a method of the map element, that was created by Leaflet (L object):
-        map.on('click', function (mapE) {
-            mapEvent = mapE;
-            form.classList.remove('hidden');
-            inputDistance.focus();
-
-
-        })
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
     }
 
 
-    _showForm() { }
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
 
-    _toggleElevationField() { }
+    _newWorkout(e) {
 
-    _newWorkout() { }
+        e.preventDefault();
+        const { lat, lng } = this.#mapEvent.latlng;
+
+        //Clear input fields:
+        inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+
+        //Display popup:
+        L.marker([lat, lng]).addTo(this.#map)
+            .bindPopup(L.popup({
+                //To view all the options for the popup, see Leaflet documentation!
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
+            .openPopup();
+    }
 }
 
 
-
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const { lat, lng } = mapEvent.latlng;
-
-    //Clear input fields:
-    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
-
-    //Display popup:
-    L.marker([lat, lng]).addTo(map)
-        .bindPopup(L.popup({
-            //To view all the options for the popup, see Leaflet documentation!
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup'
-        }))
-        .setPopupContent('Workout')
-        .openPopup();
-})
-
-inputType.addEventListener('change', function () {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
+//Creating an App object to initialize app:
+const app = new App();
