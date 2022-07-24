f257d0b46b87e6bf7a801184315ad885ad5289c3
Vitor Jaguat <vitorbutkus@gmail.com>
2022-07-24 09:25:03 -0300

    Refactor App class

--- script.js
+++ script.js
@@ -3,13 +3,7 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+
 
 class Workout {
     date = new Date();
@@ -49,14 +43,24 @@ class Cycling extends Workout {
     }
 }
 
-const run1 = new Running([39, -12], 5.2, 24, 178);
-const cycling1 = new Cycling([39, -12], 27, 95, 523);
-console.log(run1, cycling1);
+//Test:
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
 
 
 
 //////////////////////////////////////////
 // APPLICATION ARCHITETURE
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
 class App {
     #map;
     #mapEvent;
@@ -114,14 +118,39 @@ class App {
     }
 
     _newWorkout(e) {
+        //Helper functions:
+        const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp)); //only returns true if every input is a positive number
+        const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
 
         e.preventDefault();
+
+        // Get data from form:
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+
+
+
+        // If running, create running object
+        if (type === 'running') {
+            const cadence = +inputCadence.value;
+            //Check if data is valid
+            // if (!Number.isFinite(distance)) || !Number.isFinite(duration) || !Number.isFinite(cadence) //if distance, duration or cadence is NaN
+            if (!validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence))
+                return alert('Inputs have to be positive numbers')
+        }
+        //If cycling, create cycling object
+        if (type === 'cycling') {
+            const elevation = +inputElevation.value;
+
+            if (!validInputs(distance, duration, elevation) || !allPositive(distance, duration))
+                return alert('Inputs have to be positive numbers')
+        }
+        //Add new object to workouts array
+
+        //Render workout on map as a marker
         const { lat, lng } = this.#mapEvent.latlng;
-
-        //Clear input fields:
-        inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
-
-        //Display popup:
         L.marker([lat, lng]).addTo(this.#map)
             .bindPopup(L.popup({
                 //To view all the options for the popup, see Leaflet documentation!
@@ -133,6 +162,15 @@ class App {
             }))
             .setPopupContent('Workout')
             .openPopup();
+
+
+        //Render workout on list
+
+        //Hide form + clear input fields
+        inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+
+
+
     }
 }
 
